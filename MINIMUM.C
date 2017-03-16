24e44c86c9cec024fc5743c2fe9eaf1fb46776b8
VINOTH-7 <vinoth986461@gmail.com>
2017-03-16 12:54:34 +0530

    Create MINIMUM.C

---
+++ MINIMUM.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int mini(int a, int k);
+int main()
+{
+int a,k;
+printf("Enter the number:");
+scanf("%d",&a);
+printf("Enter the k value:");
+scanf("%d",&k);
+printf("Minimum:%d",mini(a,k));
+return 0;
+}
+int mini(int a ,int k)
+{
+    int b,c;
+    if(k<=0)
+return a;
+if(k>0)
+{
+    b=mini(a/10,k);
+b=(b*10)+(a%10);
+c=mini(a/10,k-1);
+}
+if(b<c)
+return b;
+else
+return c;
+}
